58acf00a59c2c7091d92c95f982b9279722580be
hidaken991018 <ed.hidaken@gmail.com>
2024-06-23 16:20:28 +0900

    no message

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,78 +1,78 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "amplifysamplepython": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    },
-    "amplifysamplepythonv2": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    },
-    "apiddfc4c6e": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    }
-  },
-  "auth": {
-    "0211amplifyclone": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
-  "function": {
-    "0211amplify": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "0211amplifyclone9f91ff9a": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "amplifysamplepython": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "amplifysamplepythonv2": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  },
-  "hosting": {
-    "S3AndCloudFront": {
-      "CloudFrontDistributionID": "string",
-      "CloudFrontDomainName": "string",
-      "CloudFrontOriginAccessIdentity": "string",
-      "CloudFrontSecureURL": "string",
-      "HostingBucketName": "string",
-      "Region": "string",
-      "S3BucketSecureURL": "string",
-      "WebsiteURL": "string"
-    }
-  },
-  "storage": {
-    "amplifyclone": {
-      "BucketName": "string",
-      "Region": "string"
-    }
-  }
-}
+  api: {
+    amplifysamplepython: {
+      ApiId: 'string';
+      ApiName: 'string';
+      RootUrl: 'string';
+    };
+    amplifysamplepythonv2: {
+      ApiId: 'string';
+      ApiName: 'string';
+      RootUrl: 'string';
+    };
+    apiddfc4c6e: {
+      ApiId: 'string';
+      ApiName: 'string';
+      RootUrl: 'string';
+    };
+  };
+  auth: {
+    '0211amplifyclone': {
+      AppClientID: 'string';
+      AppClientIDWeb: 'string';
+      IdentityPoolId: 'string';
+      IdentityPoolName: 'string';
+      UserPoolArn: 'string';
+      UserPoolId: 'string';
+      UserPoolName: 'string';
+    };
+  };
+  function: {
+    '0211amplify': {
+      Arn: 'string';
+      LambdaExecutionRole: 'string';
+      LambdaExecutionRoleArn: 'string';
+      Name: 'string';
+      Region: 'string';
+    };
+    '0211amplifyclone9f91ff9a': {
+      Arn: 'string';
+      LambdaExecutionRole: 'string';
+      LambdaExecutionRoleArn: 'string';
+      Name: 'string';
+      Region: 'string';
+    };
+    amplifysamplepython: {
+      Arn: 'string';
+      LambdaExecutionRole: 'string';
+      LambdaExecutionRoleArn: 'string';
+      Name: 'string';
+      Region: 'string';
+    };
+    amplifysamplepythonv2: {
+      Arn: 'string';
+      LambdaExecutionRole: 'string';
+      LambdaExecutionRoleArn: 'string';
+      Name: 'string';
+      Region: 'string';
+    };
+  };
+  hosting: {
+    S3AndCloudFront: {
+      CloudFrontDistributionID: 'string';
+      CloudFrontDomainName: 'string';
+      CloudFrontOriginAccessIdentity: 'string';
+      CloudFrontSecureURL: 'string';
+      HostingBucketName: 'string';
+      Region: 'string';
+      S3BucketSecureURL: 'string';
+      WebsiteURL: 'string';
+    };
+  };
+  storage: {
+    amplifyclone: {
+      BucketName: 'string';
+      Region: 'string';
+    };
+  };
+};
